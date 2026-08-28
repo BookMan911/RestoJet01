5910d0cc69f3576320d8a4976b26bdadebee5c0b
bookman <l1m2t3h4.6@gmail.com>
2026-08-28 13:04:16 -0700

    first_101

--- Core/RestaurantService.cs
+++ Core/RestaurantService.cs
@@ -9,6 +9,8 @@ namespace RestoJett.Core
     {
         public JMenu MainMenu { get; set; }
 
+        //rewrite the service to be more like asp.net core service style and use dependency injection and make it more modular and easy to test and maintain
+        // enable /add /edit /remove for everything with follwing rules
         // here is the service its asp.net style service to manage ( meals , users, customers,orders)
         // the method style is result error style like
       //  public Tuple<Exception,List<JUser>> getUsers(){} all operations like that to ease of trace errors
@@ -16,7 +18,8 @@ namespace RestoJett.Core
       // public Tuple<Exception,List<JUser>> getUsers(Juser loggeduser){} like this 
       // every action for each user is logged and saved so admin can know who did what
       // ceate cshtml page (RPAGE) that controls the service , and make that page modren UI
-      // the RPAGE 
+      // the RPAGE uses the Languageservice in top most we do ( langservice.For("lang")) and for each word in the page we use ( langservice.Transilate("word"))
+
         
     }
 }
